539a919e0e685d20b28f7ffa552dfa8ff6473de2
Lukas Gierth <lukas.gierth@mailbox.org>
2016-12-14 16:17:09 +0100

    add for-schleife

--- src/de/hsbo/geo/simsamples/cellularautomata/instances/Migration.java
+++ src/de/hsbo/geo/simsamples/cellularautomata/instances/Migration.java
@@ -2,7 +2,6 @@ package de.hsbo.geo.simsamples.cellularautomata.instances;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 
 import de.hsbo.geo.simsamples.cellularautomata.Cell;
 import de.hsbo.geo.simsamples.cellularautomata.DiscreteStateSet;
@@ -15,13 +14,25 @@ import de.hsbo.geo.simsamples.cellularautomata.TransitionFunction;
  * @author Lukas Gierth, Matthias Hensen
  */
 
-public class Migration extends TransitionFunction 
-{
+public class Migration extends TransitionFunction {
+	
+	double vX;
+	double vO;
+	double eX;
+	double eO;
+	
+	public Migration(double vX, double eX, double vO, double eO){
+		this.vX = vX;
+		this.vO = vO;
+		this.eX = eX;
+		this.eO = eO;
+	}
+	
 	@Override
 	public void defineStates() {
 		
 		// Define states: blank, population X, population Y
-		this.states = new DiscreteStateSet(".", "X", "Y");
+		this.states = new DiscreteStateSet(".", "X", "O");
 	}
 
 	@Override
@@ -34,62 +45,70 @@ public class Migration extends TransitionFunction
 			// Get 4 neighbors and choose one randomly
 			List<Cell> neighbors = c.getNeighbors(NeighborhoodIndex.NEIGH_4());
 			Collections.shuffle(neighbors);
-			Cell c2 = neighbors.get(0);
-			
-			double vermehrungsrate = 0.7;
-			double verdraengungsrate = 0.6;
-			double d = Math.random();
-	
-			// Get values for cell and neighbor cell
-			String valC2 = (String) c2.getValue(ti);
-			
-			// Vermehrung
-			if (valC == valC2){
-				if (d < vermehrungsrate){
-					
-					List<Cell> neighbors2 = c2.getNeighbors(NeighborhoodIndex.NEIGH_4());
-					Collections.shuffle(neighbors2);
-					
-					int size = neighbors2.size();
 
-					for (int i = 0; i < size; i++){
-						
-						if ((String) neighbors2.get(i).getValue(ti) == "."){
-							neighbors2.get(i).setValue(ti + 1, valC2);
-							System.out.println("BREAK: "+ i + "   SIZE: "+size);
-							break;
-						}
-					}
-					
-					return;
-				}
-				else {}
-			}
 			
-			// Migration
-			if (valC2 == "."){
+			for (int j = 0; j<neighbors.size(); j++){
 				
-				c2.setValue(ti + 1, valC);
-				c.setValue(ti + 1, ".");
-				return;
-			}
+				Cell c2 = neighbors.get(j);
+				String valC2 = (String) c2.getValue(ti);
 			
-			// Verdraengung
-			else if (valC2 != valC){
-				if (d < verdraengungsrate){
+				double d = Math.random();
+				
+				// Migration
+				if (valC2 == "."){
 					
 					c2.setValue(ti + 1, valC);
-					c.setValue(ti + 1, ".");
+					c.setValue(ti + 1, valC2);
 					return;
 				}
 				
-				else {
-					c.setValue(ti + 1, ".");
+				// Vermehrung
+				else if (valC == valC2){
+					
+					double v;
+					if (valC == "X"){
+						v = vX;
+					}
+					else{
+						v = vO;
+					}
+					
+					if (d < v){
+						
+						List<Cell> neighbors2 = c2.getNeighbors(NeighborhoodIndex.NEIGH_4());
+						Collections.shuffle(neighbors2);
+						
+						int size = neighbors2.size();
+	
+						for (int i = 0; i < size; i++){
+							
+							if ((String) neighbors2.get(i).getValue(ti) == "."){
+								neighbors2.get(i).setValue(ti + 1, valC2);
+								System.out.println("BREAK: "+ i + "   SIZE: "+size);
+								return;
+							}
+						}
+					}
+				}
+				
+				// Verdraengung
+				else if (valC2 != valC){
+					
+					double e;
+					if (valC == "X"){
+						e = eX;
+					}
+					else {
+						e = eO;
+					}
+					
+					if (d < e){
+						c2.setValue(ti + 1, valC);
+						c.setValue(ti + 1, ".");
+						return;
+					}
 				}
 			}
 		}
-		
-		else {}
-		
 	}
 }
